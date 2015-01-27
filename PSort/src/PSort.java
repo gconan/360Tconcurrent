1afafe9639530f848e938c7f0cc13ca9bcdee9bb
Michael <MichaelWehrmeister@gmail.com>
2015-01-27 13:29:45 -0600

    added test.java for main method and possibly finished quicksort. need to add threads

--- PSort/src/PSort.java
+++ PSort/src/PSort.java
@@ -2,6 +2,58 @@
 public class PSort {
 	public static void parallelSort(int[] A, int begin, int end) {
 		//your implementation goes here.
+		System.out.println("hello");
+		if(begin == end){
+			return;
+		}
+		int[] less =  new int[10000];
+		int[] greater = new int[10000];
+		int pivot = (begin+end)/2;
+		int i = begin;
+		int j = end;
+		int temp;
+		int l = 0;
+		int g = 0;
+		while(i <= j){
+			while(A[i] < A[pivot]){
+				i++;
+			}
+			while(A[j] > A[pivot]){
+				j--;
+			}
+			if(i <= j){
+				temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+				i++;
+				j--;
+			}
+		}
+		
+		if(begin < i-1){
+			parallelSort(A, begin, i-1);
+		}
+		if(i < end){
+			parallelSort(A, i, end);
+		}
+		/*while(i <= end){
+			if(A[i] < pivot){
+				less[l] = A[i];
+				l++;
+				i++;
+			}
+			if(A[i] > pivot){
+				greater[g] = A[i];
+				g++;
+				i++;
+			}
+			
+		}*/
+		return;
 	}
+	
+	
+	
+	
 
 }
