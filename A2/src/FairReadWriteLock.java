82e9d2ce79b71238953569e3f7d1d816c48abcbe
gconan <gconan@utexas.edu>
2015-02-18 10:55:06 -0600

    forgot to save fairreadwritelock

--- A2/src/FairReadWriteLock.java
+++ A2/src/FairReadWriteLock.java
@@ -6,15 +6,13 @@ import java.util.concurrent.Semaphore;
  * a) There is no read-write or write-write conflicts
  * 
  * b) A writer thread that invokes beginWrite() will be blocked until 
- * all preceding reader and writer threads have acquired and released the lock.
+ * 		all preceding reader and writer threads have acquired and released the lock.
  * 
  * c) A reader thread that invokes beginRead() will be blocked until 
- * all preceding writer threads have acquired and released the lock.
+ * 		all preceding writer threads have acquired and released the lock.
  * 
  * d) A reader thread cannot be blocked if all preceding writer threads 
- * have acquired and released the lock or no preceding writer thread exists.
- * 
- *
+ * 		have acquired and released the lock or no preceding writer thread exists.
  */
 public class FairReadWriteLock {
 	
