d968326406d9f461b02d8157dfcd23ca3e67d8d9
gconan <gconan@utexas.edu>
2015-02-18 11:28:31 -0600

    i dont think the thread classes works either, not sure how they want us to set these classes up

--- A2/src/Driver.java
+++ A2/src/Driver.java
@@ -3,10 +3,14 @@ public class Driver {
 	
 	public static void main (String[] args){
 		Garden g = new Garden(100);
-		Thread newton = new Thread();
-		Thread benji = new Thread();
-		Thread mary = new Thread();
+		
 		
 	}
 
+	
+	Thread Newton{
+		while(){
+			
+		}
+	}
 }
--- A2/src/Newton.java
+++ A2/src/Newton.java
@@ -1,4 +1,29 @@
 
-public class Newton extends Thread {
+public class Newton implements Runnable {
+	private static Newton instance = new Newton(); //singleton
+	
+	
+	private Thread newt;
+	private Boolean keepDigging;
+	
+	
+	private Newton(){
+		newt = new Thread();
+		keepDigging = false;
+	}
+	
+	public static Newton getInstance(){
+		return instance;
+	}
+
+	@Override
+	public void run() {
+		
+		while(keepDigging){
+			
+			
+		}
+		
+	}
 
 }
