966d4749f199094ecb6549e1e66b6b61f332e2a4
Michael <MichaelWehrmeister@gmail.com>
2015-02-18 21:47:11 -0600

    4 tests working

--- A2/src/CyclicBarrier.java
+++ A2/src/CyclicBarrier.java
@@ -6,6 +6,8 @@ public class CyclicBarrier {
 	private final int numberOfParties;
 	private int partiesArrived;
 	private Semaphore mutex;
+	private Semaphore everyone;
+	private Semaphore resetting;
 
 	/**
 	 * creates a new cyclicbarrier that will trip when the given number of parties (thread) are waiting upon it
@@ -14,6 +16,9 @@ public class CyclicBarrier {
 	public CyclicBarrier (int parties){
 		numberOfParties = parties;
 		mutex = new Semaphore(1);
+		partiesArrived = 0;
+		everyone = new Semaphore(1);
+		resetting = new Semaphore(1);
 	}
 	
 	/**
@@ -27,16 +32,28 @@ public class CyclicBarrier {
 	 */
 	int await() throws InterruptedException{
 		mutex.acquire();
+		
+		//if(partiesArrived == 0) { //first to arrive
+		//	everyone.acquire(); //note, first one will try to acquire everyone TWICE, this is intended
+		//}
+		
 		partiesArrived++;
-		mutex.release();
-		if(numberOfParties-partiesArrived==0){
-			notifyAll();
-			partiesArrived = 0;
+		int result = numberOfParties - partiesArrived;
+		//mutex.release();
+		if(numberOfParties - partiesArrived == 0){
+			//notifyAll();
+			partiesArrived--;
+			//everyone.release();
+			mutex.release();
 			return 0;
-		}else{
-			wait();
 		}
-		return numberOfParties-partiesArrived;
+		mutex.release();
+		
+		//everyone.acquire(); //wait for everyone before exiting
+		partiesArrived++;
+		//everyone.release();
+
+		return result;
 		
 	}
 }
