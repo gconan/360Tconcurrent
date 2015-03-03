d23ef011884a4a782aa82655406d9f2baf7c0d83
gconan <gconan@utexas.edu>
2015-03-03 11:34:40 -0600

    started server stuff. created main that inits a server. String parsing of server configureation information. working on TCPmonitor and UDPmonitor for listening to the sockets for incoming client calls

--- A3/src/QueueTest.java
+++ A3/src/QueueTest.java
@@ -2,21 +2,32 @@
 public class QueueTest {
 	
 	public static void main(String [] args) {
-		PQueue pq = new PQueue(10);
-		pq.insert("n1", 4);
+		PQueue pq = new PQueue(5);
+		System.out.println("Inser n1 at pos "+pq.insert("n1", 4));
 		pq.printQueue();
-		pq.insert("n2", 2);
+		System.out.println("Inser n2 at pos "+pq.insert("n2", 2));
+		System.out.println("Inser n2 at pos "+ pq.insert("n2", 7));
 		pq.printQueue();
-		pq.insert("n3", 8);
+		System.out.println("Inser n3 at pos "+ pq.insert("n3", 8));
 		pq.printQueue();
-		pq.insert("n4", 3);
+		System.out.println("Inser n5 at pos "+ pq.insert("n5", 8));
 		pq.printQueue();
-		//int loc = pq.search("n4");
-		//System.out.println(loc);
-		//pq.getFirst();
-		//pq.printQueue();
-//		loc = pq.search("n4");
-//		System.out.println(loc);
+		System.out.println("Inser n4 at pos "+ pq.insert("n4", 3));
+		pq.printQueue();
+		System.out.println("Inser n6 at pos "+ pq.insert("n6", 4));
+		pq.printQueue();
+		int loc = pq.search("n4");
+		System.out.println(loc);
+		pq.getFirst();
+		pq.printQueue();
+		loc = pq.search("n4");
+		System.out.println("n4 location "+loc);
+		loc = pq.search("n3");
+		System.out.println("n3 location "+loc);
+		loc = pq.search("n1");
+		System.out.println("n1 location "+loc);
+		loc = pq.search("n2");
+		System.out.println("n2 location "+loc);
 	}
 	
 
--- A3/src/Server.java
+++ A3/src/Server.java
@@ -1,4 +1,76 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+
 
 public class Server {
+	private ServerSocket TCPSocket;
+	private DatagramSocket UDPSocket;
+	private HashMap<Integer, String> library = new HashMap<Integer, String>();
+	private ExecutorService pool;
+	
+	public Server(){
+		Scanner scan = new Scanner(System.in);	//use "standard input"
+		try{
+			this.configureServer(scan.nextLine());
+		}catch(Exception e){
+			System.out.println(e.getMessage());	//use "standard output"
+			e.printStackTrace();
+		}finally{
+			scan.close();
+		}
+	}
+
+	private void configureServer(String configString) throws NumberFormatException, IOException {
+		
+		String[] configArgs = configString.split(" ");
+		
+		//initialize our library
+			//trim in case of extra white space added by sloppy user
+		for(int i=0; i<Integer.parseInt(configArgs[0].trim()); i++){
+			library.put(i, "available");
+		}
+		
+		TCPSocket = new ServerSocket(Integer.parseInt(configArgs[1].trim()));
+		UDPSocket = new DatagramSocket(Integer.parseInt(configArgs[2].trim()));
+		
+		
+	}
+		
+	private void openDoorsForBusiness() {
+		//create listeners on both TCP and UDP and let the client requests flow
+		
+	}
+	
+//********************************************************NESTED CLASS: TCP MONITOR*********************************************
+	
+	protected class TCPmonitor implements Runnable{
+		
+		protected TCPmonitor(Socket sock){
+			
+		}
+
+		@Override
+		public void run() {
+			
+		}
+	}
+	
+	
+	
+	
+//***************************************************MAIN FUNCTION FOR RUNNING FROM COMMMAND LINE*******************************
+	/**
+	 * starts the server and allows it to run requests
+	 * @param args
+	 */
+	public static void main(String[] args){
+		Server server = new Server();
+		server.openDoorsForBusiness();
+	}
 
 }
