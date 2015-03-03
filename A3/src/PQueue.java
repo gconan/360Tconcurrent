2369a63ede90bf4412e2c00a727bedca85c747e9
Michael <MichaelWehrmeister@gmail.com>
2015-03-02 19:37:06 -0600

    might have finished insert

--- A3/src/PQueue.java
+++ A3/src/PQueue.java
@@ -12,10 +12,17 @@ public class PQueue {
 
 	public int insert(String name, int priority){ 
 		// Inserts the name with its priority in the PQueue. // It returns -1 if the name is already present in the list. // Otherwise, returns the current position in the list where the name was inserted. // This method blocks when the list is full.
+		Node node = new Node(name, priority);
 		if(search(name) == -1){
 			return -1;
-		} else if(size >= maxSize){
+		} 
+		waituntil(size < maxSize);
+		if(size >= maxSize){
 			//wait
+			return 0;
+		} else if(size == 0){
+			head = node;
+			size++;
 			return 0;
 		} else{
 			
