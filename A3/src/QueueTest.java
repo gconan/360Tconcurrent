2f9524590b695cb50391441ea6449331c3ac23ac
gconan <gconan@utexas.edu>
2015-03-02 20:10:32 -0600

    runner

---
+++ A3/src/QueueTest.java
@@ -0,0 +1,24 @@
+
+public class QueueTest {
+	
+	public static void main(String [] args) {
+		PQueue pq = new PQueue(10);
+		pq.insert("n1", 4);
+		pq.printQueue();
+		pq.insert("n2", 2);
+		pq.printQueue();
+		pq.insert("n3", 8);
+		pq.printQueue();
+		pq.insert("n4", 3);
+		pq.printQueue();
+		//int loc = pq.search("n4");
+		//System.out.println(loc);
+		//pq.getFirst();
+		//pq.printQueue();
+//		loc = pq.search("n4");
+//		System.out.println(loc);
+	}
+	
+
+
+}
