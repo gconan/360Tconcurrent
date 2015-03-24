dff944bb96e81c295c9fa50f4f0f99348e01d0c0
gconan <gconan@utexas.edu>
2015-03-24 11:31:15 -0500

    crash is done, need to test

--- A4/src/TestClientInput.java
+++ A4/src/TestClientInput.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 public class TestClientInput {
 
 	@Test
-<<<<<<< HEAD
 	public void test() {
 		String file = "c1 2" +"\n"+ "127.0.0.1:8025" +"\n"+ "128.0.0.1:8030" +"\n"+ "crash 3 3000";
 		Client testclient = new Client();
@@ -19,8 +18,8 @@ public class TestClientInput {
 		
 		
 		assertTrue(result.equals("Server 1 is on IP: /127.0.0.1 and port number 8025 Server 2 is on IP: /128.0.0.1 and port number 8030 "));
+	}
 		
-=======
 	public void testSimpleInputFile() {
 		String file = "1 2" +"\n"+ "127.0.0.1:8080" +"\n"+ "128.0.0.1:9005";
 		Scanner scan = new Scanner(file);
@@ -28,7 +27,6 @@ public class TestClientInput {
 		String result = server.printReplicaSet();
 		
 		assertTrue(result.equals("Server 1 is on IP: /127.0.0.1 and port number 8080 Server 2 is on IP: /128.0.0.1 and port number 9005 "));
->>>>>>> 835c6f86be7e50dd20cd217427334293a4d62cd6
 	}
 
 }
