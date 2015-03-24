46e611ee255d22a760ed9510ac958ca2314af0ae
Michael <MichaelWehrmeister@gmail.com>
2015-03-24 10:57:09 -0500

    client input tests

--- A4/src/TestClientInput.java
+++ A4/src/TestClientInput.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Test;
 
 
@@ -7,7 +9,16 @@ public class TestClientInput {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String file = "c1 2" +"\n"+ "127.0.0.1:8025" +"\n"+ "128.0.0.1:8030" +"\n"+ "crash 3 3000";
+		Client testclient = new Client();
+		testclient.inputFirstLine("c1 2");	
+		testclient.inputLines("127.0.0.1:8025", 2, 0);
+		testclient.inputLines("128.0.0.1:8030", 2, 1);
+		String result = testclient.printReplicaSet();
+		
+		
+		assertTrue(result.equals("Server 1 is on IP: /127.0.0.1 and port number 8025 Server 2 is on IP: /128.0.0.1 and port number 8030 "));
+		
 	}
 
 }
